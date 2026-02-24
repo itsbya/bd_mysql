0856dff33bb453e09724833baaf240a3f1a75799
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-02-24 15:33:15 -0300

    :tada: commit inicial

---
+++ aula_02/my.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE db_quitanda;
+
+USE db_quitanda;
+
+CREATE TABLE tb_produtos(
+id bigint auto_increment,
+nome varchar(255) NOT NULL,
+quantidade int,
+data_validade date,
+preco decimal NOT NULL,
+PRIMARY KEY(id)
+);
+
+-- Comanod de Manipulação de dados
+INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
+VALUES ("tomate", 1000, "2026-03-24", 8.99);
+
+INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
+VALUES ("banana", 15000, "2026-03-04", 12.50),
+("morango", 15000, "2026-03-04", 22.00),
+("alface", 500, "2026-03-04", 12.50),
+("beterraba", 4000, "2026-03-04", 9.50);
+
+
+SELECT * FROM tb_produtos;
+
+SELECT * FROM tb_produtos WHERE id = 2;
+
+SELECT * FROM tb_produtos WHERE preco > 10.00;
+
+SELECT * FROM tb_produtos WHERE preco < 10.00;
+
+SELECT * FROM tb_produtos WHERE nome = "banana" OR preco > 20.00;
+
+SELECT * FROM tb_produtos WHERE nome = "banana" AND preco = 12.50;
+
+SELECT * FROM tb_produtos WHERE nome != "banana";
+
+SELECT nome, DATE_FORMAT(data_validade, '%d/%m/%y') AS Data_Validade, CONCAT('R$', FORMAT(preco, 2, 'pt_BR'))
+AS preco FROM 
+tb_produtos;
+
+
+UPDATE tb_produtos SET preco = 8.99 WHERE id = 1 OR id = 2;
+
+DELETE FROM tb_produtos WHERE id = 6;
+
+SET SQL_SAFE_UPDATE = 1;
+
+
+-- Comandos Estruturais
+ALTER TABLE tb_produtos MODIFY preco decimal(6, 2);
+
+ALTER TABLE tb_produtos ADD descricao varchar(500);
+
+ALTER TABLE tb_produtos CHANGE descricao descricao_produto varchar(500);
